41c909f74f9a61dc2242dbcfda81a5f36dcab896
marinacor1 <marinaspam1234@gmail.com>
2016-06-01 09:44:06 -0600

    Show test passing

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,4 +5,8 @@ class Api::V1::ItemsController < ApplicationController
   def index
     respond_with Item.all
   end
+
+  def show
+    respond_with Item.find(params[:id])
+  end
 end
--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -3,8 +3,22 @@ require 'test_helper'
 class Api::V1::ItemsControllerTest < ActionController::TestCase
   test "can visit index" do
     get "index", format: :json
-    # parsed_json = JSON.parse(response.body)
+    parsed_json = JSON.parse(response.body)
 
     assert_response :success
+
+    assert_equal 2, parsed_json.count
+    assert parsed_json.first.has_key?("description")
+
   end
+
+  test "can visit show" do
+    id = Item.first.id
+    get :show, id: id, format: :json
+    item = JSON.parse(response.body)
+
+    assert_response :success
+
+    assert_equal item["id"], id
+    end
 end
